2674ba466964b60cf022b03d16f48ac231c81169
Blaise Laflamme <blaise@laflamme.org>
2012-09-11 21:57:44 -0400

    changed docs theme for pylons

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ else:
 
 sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
-html_theme = 'pyramid'
+html_theme = 'pylons'
 html_theme_options = dict(github_url='https://github.com/Pylons/acidfs')
 
 # Theme options are theme-specific and customize the look and feel of a theme
